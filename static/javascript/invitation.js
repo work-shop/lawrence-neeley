c8d3b469a0c1c4cac2d2ee0d7a82f89da62ac17a
Nic Schumann <nicolas_schumann@brown.edu>
2016-01-05 10:14:25 -0500

    implemented localstorage consistency for the invitation

--- static/javascript/invitation.js
+++ static/javascript/invitation.js
@@ -1,34 +1,78 @@
 var $ = require('jquery');
-
-$( '.internal' )
-	.on( 'mouseover', function() {
-		var e = $(this), attr = e.attr('data-invitation');
-
-		if ( typeof attr !== "undefined" && attr ) {
-
-			var invitation = $('#invitation');
-
-			invitation.css({
-
-				position:'absolute',
-				top: e.offset().top + "px",
-				left: e.offset().left + e.width() + 10 + "px"
-
-			});
-
-			invitation.find('h5').text( e.data('invitation') ).removeClass('off').addClass('on');
-
-		}
+var store = require('store2'), activeKey = "dar-invitation-active";
 
 
-	})
-	.on('mouseout', function() {
-		var e = $(this), attr = e.attr('data-invitation');
+function invitationMouseover() {
+	var e = $(this), attr = e.attr('data-invitation');
 
-		if ( typeof attr !== "undefined" && attr ) {
+	if ( typeof attr !== "undefined" && attr ) {
 
-			$( '#invitation' ).find('h5').removeClass('on').addClass('off').text('');
+		var invitation = $('#invitation');
 
-		}
+		invitation.css({
 
-	});
+			position:'absolute',
+			top: e.offset().top + "px",
+			left: e.offset().left + e.width() + 10 + "px"
+
+		});
+
+		invitation.find('h5').text( e.data('invitation') ).removeClass('off').addClass('on');
+
+	}
+
+}
+
+function invitationMouseout() {
+	var e = $(this), attr = e.attr('data-invitation');
+
+	if ( typeof attr !== "undefined" && attr ) {
+
+		$( '#invitation' ).find('h5').removeClass('on').addClass('off').text('');
+
+	}
+
+}
+
+function invitationHandlers( active ) {
+	console.log( "invitationHandlers" );
+	console.log( active );
+
+	if ( typeof active == "undefined" ) {
+
+		store.set(activeKey, true);
+
+	} else {
+
+		store.set(activeKey, active);
+
+	}
+
+	console.log( active );
+	console.log( store.get( activeKey ) );
+
+	if ( active  ) {
+
+		$( '#invitation-button' ).addClass('active');
+
+		$( '.internal' )
+			.on( 'mouseover', invitationMouseover )
+			.on('mouseout', invitationMouseout );
+
+	} else {	
+
+		$( '#invitation-button' ).removeClass('active');
+
+		$( '.internal' )
+			.off('mouseover').off('mouseout');
+			//.off( 'mouseover', invitationMouseover )
+			//.off( 'mouseout', invitationMouseout );
+
+	}
+
+}
+
+
+$('#invitation-button').on('click', function() { invitationHandlers( !store.get( activeKey ) ); });
+
+invitationHandlers( store.get( activeKey ) );
